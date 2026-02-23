555a03a9af5734d44afc26a7196a263412a54677
Kriszchan <panyik.krisztian@students.jedlik.eu>
2026-02-23 12:21:25 +0100

    login and logout service functions completed

--- src/services/APIservice.tsx
+++ src/services/APIservice.tsx
@@ -1,6 +1,11 @@
 const API_URL = "http://bufeapi-markomilan.jcloud.jedlik.cloud/api";
 
-
+const getCookie = (name: string) => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop()?.split(';').shift();
+  return null;
+};
 export const Login = async (postData: any) => {
   try {
     const response = await fetch(`${API_URL}/account/login`, {
@@ -15,18 +20,20 @@ export const Login = async (postData: any) => {
       throw new Error(`Error: ${response.status} ${response.statusText}`);
     }
     const data = await response.json();
-    
+    document.cookie = `token=${data.access_token}; expires= ${(new Date(Date.now() + 1000*60*60*24*30)).toUTCString()}; path=/`
+    return data;
   } catch (error) {
     throw error;
   }
 };
   export const Logout = async () => {
   try {
+    const token = getCookie('token');
     const response = await fetch(`${API_URL}/account/logout`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `${document.cookie}`,
+        "Authorization": `Bearer ${token}`,
       },
     });
 
@@ -34,7 +41,8 @@ export const Login = async (postData: any) => {
       throw new Error(`Error: ${response.status} ${response.statusText}`);
     }
     const data = await response.json();
-
+    document.cookie = `token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
+    return data;
     
   } catch (error) {
     throw error;
