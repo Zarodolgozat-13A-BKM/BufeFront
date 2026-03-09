49c98c00d7d4f8245b97a77665dd5ba0691a9fe8
kriszchan <panyik.krisztian@students.jedlik.eu>
2026-03-09 16:17:15 +0100

    feat: add account page with routing and profile functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,35 @@ import './App.css'
 import MainPage from './pages/MainPage'
 import { useAppSelector } from './store/hooks'
 import LoginPage from './pages/LoginPage'
-import { Provider } from 'react-redux'
-import { store } from './store/store'
-
-function AppContent() {
-  const { isLoggedIn } = useAppSelector((state) => state.auth)
-
-  return isLoggedIn ? <MainPage /> : <LoginPage />
-}
+import ProfilePage from './pages/ProfilePage'
+import { Navigate, Route, Routes } from 'react-router'
 
 function App() {
+  const { isLoggedIn } = useAppSelector((state) => state.auth)
 
   return (
-    <Provider store={store}>
-      <AppContent />
-    </Provider>
+    <Routes>
+      <Route
+        path="/"
+        element={<Navigate to={isLoggedIn ? '/main' : '/login'} replace />}
+      />
+      <Route
+        path="/login"
+        element={isLoggedIn ? <Navigate to="/main" replace /> : <LoginPage />}
+      />
+      <Route
+        path="/main"
+        element={isLoggedIn ? <MainPage /> : <Navigate to="/login" replace />}
+      />
+      <Route 
+        path="/me"
+        element={isLoggedIn ? <ProfilePage /> : <Navigate to="/login" replace />}
+      />
+      <Route
+        path="*"
+        element={<Navigate to={isLoggedIn ? '/main' : '/login'} replace />}
+      />
+    </Routes>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
 import { store } from './store/store'
