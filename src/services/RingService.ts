4429d3c2dc1482849fe25b34e1b9023706c6a952
Kriszchan <panyik.krisztian@students.jedlik.eu>
2026-03-12 09:14:11 +0100

    feat: update API_URL in RingService to use the correct endpoint for ring-list retrieval

--- src/services/RingService.ts
+++ src/services/RingService.ts
@@ -1,6 +1,6 @@
 import type { RingModel } from "../Models/RingModel";
 import axios from "axios";
-const API_URL = import.meta.env.VITE_RING_API_URL || "https://jcs.jedlik.eu:4001/api";
+const API_URL = import.meta.env.VITE_RING_API_URL || `https://jedlikinfo.jedlik.eu/api/api/timetable/ringsystem/${new Date().toISOString().split("T")[0]}`;
 export const GetRinging = async () => {
   const response = await axios.get<RingModel>(`${API_URL}/ring-list`);
   return response.data;
