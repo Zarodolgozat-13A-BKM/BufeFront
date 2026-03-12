6f9b7e2bb5ea8268e209fd178ba2c0434ecd1ef3
Kriszchan <panyik.krisztian@students.jedlik.eu>
2026-03-12 09:17:02 +0100

    fix: update API request in GetRinging to use the correct endpoint for ring data retrieval

--- src/services/RingService.ts
+++ src/services/RingService.ts
@@ -1,7 +1,7 @@
 import type { RingModel } from "../Models/RingModel";
 import axios from "axios";
-const API_URL = import.meta.env.VITE_RING_API_URL || `https://jedlikinfo.jedlik.eu/api/api/timetable/ringsystem/${new Date().toISOString().split("T")[0]}`;
+const API_URL = import.meta.env.VITE_RING_API_URL || `https://jedlikinfo.jedlik.eu/api/api/`;
 export const GetRinging = async () => {
-  const response = await axios.get<RingModel>(`${API_URL}/ring-list`);
+  const response = await axios.get<RingModel>(`${API_URL}timetable/ringsystem/${new Date().toISOString().split("T")[0]}`);
   return response.data;
 };
