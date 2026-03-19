4212b89424c1641c03cdc173c4c31540f8e09e28
milanmarko <marko.milan@students.jedlik.eu>
2026-03-19 12:16:30 +0100

    Update CI/CD workflow branch and fix environment variable references in Echo configuration

--- src/lib/echo.ts
+++ src/lib/echo.ts
@@ -6,13 +6,13 @@ window.Pusher = Pusher;
 
 export const echo = new Echo({
   broadcaster: "reverb",
-  key: import.meta.env.VITE_REVERB_APP_KEY,
-  wsHost: import.meta.env.VITE_REVERB_HOST ?? "localhost",
-  wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-  wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-  forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? "https") === "https",
+  key: import.meta.env.REVERB_APP_KEY,
+  wsHost: import.meta.env.REVERB_HOST ?? "localhost",
+  wsPort: import.meta.env.REVERB_PORT ?? 8080,
+  wssPort: import.meta.env.REVERB_PORT ?? 443,
+  forceTLS: (import.meta.env.REVERB_SCHEME ?? "https") === "https",
   enabledTransports: ["ws", "wss"],
-  authEndpoint: import.meta.env.VITE_REVERB_AUTH_URL,
+  authEndpoint: import.meta.env.REVERB_AUTH_URL,
   auth: {
     headers: {
       Authorization: `Bearer ${localStorage.getItem("token")}`,
