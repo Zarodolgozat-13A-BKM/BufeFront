4f6c4d62bdceabc2f18d0634a6a2d0f125beecaa
milanmarko <marko.milan@students.jedlik.eu>
2026-03-19 12:41:48 +0100

    Fix wsHost URL in Echo configuration by removing the protocol

--- src/lib/echo.ts
+++ src/lib/echo.ts
@@ -7,7 +7,7 @@ window.Pusher = Pusher;
 export const echo = new Echo({
   broadcaster: "reverb",
   key: "lkmdfbj4dsd2tyfuprvn",
-  wsHost: "https://bufeapi-ws.jcloud.jedlik.cloud",
+  wsHost: "bufeapi-ws.jcloud.jedlik.cloud",
   wsPort: import.meta.env.REVERB_PORT ?? 8080,
   wssPort: import.meta.env.REVERB_PORT ?? 443,
   forceTLS: (import.meta.env.REVERB_SCHEME ?? "https") === "https",
