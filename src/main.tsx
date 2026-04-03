913a52d7d6e97eb5607ed351cf53ac116cb5da4c
panyik <panyik.krisztian@students.jedlik.eu>
2026-04-03 13:51:17 +0200

    feat(main): disable console logs in production environment

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,16 @@ import './index.css'
 import App from './App.tsx'
 import { store, persistor } from './store/store'
 
+if (!import.meta.env.DEV) {
+  const noop = () => undefined
+  console.log = noop
+  console.info = noop
+  console.debug = noop
+  console.warn = noop
+  console.error = noop
+  console.trace = noop
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
